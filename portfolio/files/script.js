0f518242acb755619804e3d476bd30854b279d1e
Manasi <147149200+mdbhise07@users.noreply.github.com>
2025-01-01 15:08:23 +0530

    Add files via upload

---
+++ portfolio/files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer","React Developer","UI/UX Designer","Salesforce Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
